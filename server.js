21f5501902570e47544e8bb9f2ac8134cf75ad0c
mrcartoony315 <khansufyaan315@gmail.com>
2025-10-23 02:29:41 +0530

    deploy

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 // Middleware
 app.use(cors(
   {
-    origin: "https://sufyaanuddinportfolio.netlify.app/",
+    origin: ["https://sufyaanuddinportfolio.netlify.app"],
   }
 ));
 app.use(express.json());
